e5a34be6d45ed9446ad0720506f6d4aaf853d613
李熠 <liyi@banyuan.club>
2020-02-24 22:54:15 +0800

    包路径答案

---
+++ ClassBase/Package/src/club/banyuan/cal/Calculator.java
@@ -0,0 +1,8 @@
+package club.banyuan.cal;
+
+class Calculator {
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+}
---
+++ ClassBase/Package/src/club/banyuan/cal/Main.java
@@ -0,0 +1,9 @@
+package club.banyuan.cal;
+
+class Main {
+
+  public static void main(String args[]) {
+    Calculator obj = new Calculator();
+    System.out.println(obj.add(10, 20));
+  }
+}
