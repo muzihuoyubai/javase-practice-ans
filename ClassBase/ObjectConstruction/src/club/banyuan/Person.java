3a724bbbfe8515993fe2fa8e8b901bdb944700d1
李熠 <liyi@banyuan.club>
2020-02-26 15:04:27 +0800

    对象构造

---
+++ ClassBase/ObjectConstruction/src/club/banyuan/Person.java
@@ -0,0 +1,19 @@
+package club.banyuan;
+
+public class Person {
+
+  private String name;
+  private int age;
+
+  public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public Person() {
+  }
+}
